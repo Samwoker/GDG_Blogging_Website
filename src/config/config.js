35072c5195f3660868b7974743aa01eaca67077a
Samwoker <149298507+Samwoker@users.noreply.github.com>
2025-04-04 22:13:22 -0900

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const { envValidation } = require("../validation");
+
+const { value: envVars, error } = envValidation.envSchema.validate(process.env);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+module.exports = {
+  port: envVars.PORT,
+  dbConnection: envVars.DB_CONNECTION,
+};
---
+++ src/server.js
@@ -0,0 +1,14 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+const http = require("http");
+const httpServer = http.createServer(app);
+const config = require("./config/config");
+
+mongoose
+  .connect(config.dbConnection)
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log(err));
+
+const server = httpServer.listen(config.port,()=>{
+    console.log(`Server is running on port ${config.port}`);
+})  
