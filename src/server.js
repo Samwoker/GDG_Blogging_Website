8b16b6d95a466a7470b77030f2e61089b41d8e20
Kirubel Legese <124571007+Proffesorgreen@users.noreply.github.com>
2025-04-07 14:11:41 -0700

    commit_two
    Signed-off-by: Kirubel Legese  <124571007+Proffesorgreen@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 const http = require("http");
 const httpServer = http.createServer(app);
 const config = require("./config/config");
-<<<<<<< HEAD
 const profileRoutes = require('./routes/profile.routes');
-=======
 const logger = require("./config/logger");
->>>>>>> 103518734f695756a90de16a3384f6f8163a533e
 
 mongoose
   .connect(config.dbConnection,{
