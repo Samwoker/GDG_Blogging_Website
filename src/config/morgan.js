6f678d9c10be6e4fc39a1d371597a5c1375ce704
Samwoker <149298507+Samwoker@users.noreply.github.com>
2025-04-12 22:10:22 -0900

    Update morgan.js
    Signed-off-by: Samwoker <149298507+Samwoker@users.noreply.github.com>

--- src/config/morgan.js
+++ src/config/morgan.js
@@ -1,24 +1,28 @@
 const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
+const logDirectory = path.join(__dirname, "..", "logs");
+
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
 
 morgan.token("message", (req, res) => res.locals.errorMessage || "");
-
 const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "..", "logs/access.log"),
-  { flags: "a" }
+  path.join(logDirectory, "access.log"),
+  { flags: "a" } 
 );
-
 const successHandlerFormat = `:method :url :status :response-time ms :user-agent :date`;
 const errorHandlerFormat = `:method :url :status :response-time ms :user-agent :date - error-message :message`;
 
 const successHandler = morgan(successHandlerFormat, {
   stream: accessLogStream,
-  skip: (req, res) => res.statusCode >= 400,
+  skip: (req, res) => res.statusCode >= 400, 
 });
+
 const errorHandler = morgan(errorHandlerFormat, {
   stream: accessLogStream,
-  skip: (req, res) => res.statusCode < 400,
+  skip: (req, res) => res.statusCode < 400, 
 });
 
 module.exports = {
