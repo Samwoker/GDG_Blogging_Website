9d00d6d110ae99322b10ffd6a8c8c28bcaa94c3c
Samwoker <149298507+Samwoker@users.noreply.github.com>
2025-04-12 21:55:34 -0900

    Update users.js
    Signed-off-by: Samwoker <149298507+Samwoker@users.noreply.github.com>

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const mongoose = require('mongoose');
-const post= require('../models/Post');
+const post= require('../models/post');
 
 const bcrypt = require('bcrypt');
 
